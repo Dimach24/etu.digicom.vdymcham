e2490be1cfa98c3df1590d113df7936ae97fc483
Dim_Cham <dchaminov@ya.ru>
2025-10-01 22:54:19 +0300

    feat: time increasing

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -3,20 +3,24 @@ clc; clear all ;
 snrs=-3:7;
 errorRates=zeros(1,length(snrs));
 in = Simulink.SimulationInput("model1");
-time=100000;
-
+time=6000;
+maxtime=time*20;
 for i=1:length(snrs)
     in=in.setModelParameter('StopTime',num2str(time));
     snr=snrs(i);
     out=sim(in);
     errorRates(i) = out.errors(1);
+    time=min(time*2,maxtime);
     if out.errors(2)<100
-        fprintf("For snr %d got %d errors. No less than 100 expected.\n",snr,out.errors(2))
+        fprintf("For snr %d got %d errors. At least 100 expected.\n",snr,out.errors(2))
     end
     fprintf("%3d: %10.6g\n", snr,errorRates(i))
 end
 %%
-scatter(snrs,errorRates*100,Marker="x")
+scatter(snrs,errorRates,Marker="x")
 xlabel("SNR, dB")
-ylabel("Error rate, %")
+ylabel("R")
+yscale("log")
 grid on;
+save("lab1.mat","errorRates","snrs")
+saveas(gcf(),"R_SNR","fig")
