3c1cf7f8c438a1ab9749e3d3094c1861ca0fe9e0
Dim_Cham <dchaminov@ya.ru>
2025-10-09 16:21:00 +0300

    Fixed signal to noise ratio

--- lab1/lab1.m
+++ lab1/lab1.m
@@ -3,15 +3,13 @@ clc; clear all ;
 snrs=-3:7;
 errorRates=zeros(1,length(snrs));
 in = Simulink.SimulationInput("model1");
-time=6000;
-maxtime=time*20;
+time=100000;
 for i=1:length(snrs)
     in=in.setModelParameter('StopTime',num2str(time));
     snr=snrs(i);
     out=sim(in);
     errors=out.get('errors');
     errorRates(i) = errors(1);
-    time=min(time*2.4,maxtime);
     fprintf("For snr %d got %d errors. At least 100 expected.\n",snr,errors(2))
     fprintf("%3d: %10.6g\n", snr,errorRates(i))
 end
@@ -30,20 +28,16 @@ xlim([min(snrs)-1,max(snrs)+1])
 save("lab1_1.mat","errorRates","snrs")
 
 %%
-clc; clear all ;
-%%
-snrs=-3:7;
+snrs=-3:9;
 errorRates=zeros(1,length(snrs));
 in = Simulink.SimulationInput("model2");
-time=6000;
-maxtime=time*20;
+time=100000;
 for i=1:length(snrs)
     in=in.setModelParameter('StopTime',num2str(time));
     snr=snrs(i);
     out=sim(in);
     errors=out.get('errors');
     errorRates(i) = errors(1);
-    time=min(time*2.4,maxtime);
     fprintf("For snr %d got %d errors. At least 100 expected.\n",snr,errors(2))
     fprintf("%3d: %10.6g\n", snr,errorRates(i))
 end
