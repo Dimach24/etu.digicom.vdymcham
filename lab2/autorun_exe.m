dc143582b5ce7abc48262fff3b78cc44687690df
Dim_Cham <dchaminov@ya.ru>
2025-10-22 22:32:43 +0300

    another models and autorun for them

--- lab2/autorun_exe.m
+++ lab2/autorun_exe.m
@@ -1,16 +1,66 @@
 clc; close all; clear all;
+
 %%
-in = Simulink.SimulationInput("model_am");
-snrs=11:21;
-errors=zeros(1,length(snrs));
-for i=1:length(snrs)
-    snr=snrs(i);
-    EbPerNo=snr;
-    sim(in);
-    errors(i)=Errors(1);
-    fprintf("snr: %6.4g errorrate: %7.6g\n",snr,errors(i))
-end
+[snrs_am,errors_am] =run_model("am",11:21);
+
+
+%%
+[snrs_pm,errors_pm] =run_model("pm",6:16);
+%%
+[snrs_qam16,errors_qam16] =run_model("qam_gray",2:12);
 figure
-scatter(snrs,errors)
+scatter(snrs_am,errors_am,Marker="+")
+hold on;
+scatter(snrs_pm,errors_pm,Marker="x")
+scatter(snrs_qam16,errors_qam16,Marker="^")
+
 yscale(gca(),"log")
+xlabel("Eb/No")
+ylabel("P_{ош}")
+grid on
+legend( "АМ", "ФМ", "QAM")
 %%
+
+%%
+[snrs_qam_bin,errors_qam_bin] =run_model("qam_bin",2:12);
+figure
+scatter(snrs_qam16,errors_qam16,Marker="+")
+hold on;
+scatter(snrs_qam_bin,errors_qam_bin,Marker="x")
+
+yscale(gca(),"log")
+xlabel("Eb/No")
+ylabel("P_{ош}")
+grid on
+legend("Код Грея", "Последоватльный\nподсчёт")
+
+%%
+[snrs_qam64,errors_qam64] =run_model("qam_64",6:16);
+[snrs_qam4,errors_qam4] =run_model("qam_4",-2:8);
+figure
+scatter(snrs_qam4,errors_qam4,Marker="+")
+hold on;
+scatter(snrs_qam16,errors_qam16,Marker="x")
+scatter(snrs_qam64,errors_qam64,Marker="^")
+yscale(gca(),"log")
+xlabel("Eb/No")
+ylabel("P_{ош}")
+grid on
+legend( "QAM-4", "QAM-16", "QAM-64")
+%%
+
+save("lab2.mat", ...
+    "snrs_am","snrs_pm","snrs_qam4","snrs_qam16","snrs_qam_bin","snrs_qam64", ...
+    "errors_am","errors_pm","errors_qam4","errors_qam16","errors_qam_bin","errors_qam64" ...
+)
+function [snrs,errors] = run_model(name,snrs)
+    in = Simulink.SimulationInput("model_"+name);
+    errors=zeros(1,length(snrs));
+    for i=1:length(snrs)
+        snr=snrs(i);%%
+        in=in.setVariable("EbPerNo",snr);
+        out=sim(in);
+        errors(i)=out.Errors(1);
+        fprintf("snr: %6.4g errorrate: %7.6g\n",snr,errors(i))
+    end
+end
