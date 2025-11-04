50f7760c7745d1914af7e95863420431d2cfb1d7
Valentin Dymchenko <vdymch@yandex.ru>
2025-11-04 19:34:05 +0300

    theoretical script for lab4 is created

---
+++ lab4/lab4_theoretical.m
@@ -0,0 +1,54 @@
+close all
+
+names = {
+    'АБГШ-канал', ... % 4.3.1 aka AWGN
+    'Рэлеевский канал', ... % 4.3.2 aka RAYLEIGH
+    'Выбор мощного сигнала', ... % 4.3.4 aka STRONG_CHOICE
+    'Оптимальное сложение' % 4.3.5 aka OPTIUM_SUM
+};
+
+q_dB = {
+    0:2:12, ... % 4.3.1 aka AWGN 
+    0:5:30, ... % 4.3.2 aka RAYLEIGH
+    0:2:20, ... % 4.3.4 aka STRONG_CHOICE
+    0:2:14 ... % 4.3.5 aka OPTIUM_SUM
+    };
+
+q = {db2pow(q_dB{1}), db2pow(q_dB{2}), db2pow(q_dB{3}), db2pow(q_dB{4})};
+
+P_b_theor = {
+    1/2 * exp(- q{1} / 2), ... % 4.3.1 aka AWGN 
+    1 ./ (2 + q{2}), ... % 4.3.2 aka RAYLEIGH
+    4 ./ (q{3}.^2 + 6 * q{3} + 8), ... % 4.3.4 aka STRONG_CHOICE
+    0.5 * (2 ./ (q{4} + 2)) .^ 2 % 4.3.5 aka OPTIUM_SUM
+    };
+
+q0 = 1; % idk
+w_theor = {
+    1 ./ q{2} .* exp(- q{2} ./ q0),
+    2 ./ q0 .* (1 - exp(- q{3} ./ q0)) .* exp(- q{3} ./ q0)
+    };
+
+markers = {"square", "x", "+", "o"};
+
+figure
+grid on
+hold on
+for i = 1:4
+plot(q_dB{i}, log10(P_b_theor{i}),"Marker",markers{i}, "LineStyle","--")
+end
+xlabel('E_b/N_0, дБ'); ylabel("log_{10}(P_b)")
+legend( ...
+    strcat(names{1},' (теор.)'), ...
+    strcat(names{2},' (теор.)'), ...
+    strcat(names{3},' (теор.)'), ...
+    strcat(names{4},' (теор.)') ...
+    )
+
+figure
+grid on
+plot(q_dB{2}, w_theor{1},"Marker",markers{2}, "LineStyle","--")
+
+figure
+grid on
+plot(q_dB{3}, w_theor{2},"Marker",markers{2}, "LineStyle","--")
